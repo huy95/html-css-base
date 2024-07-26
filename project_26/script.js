650610e445c50485e82489097f8df6a05e44f55c
huy <huy.ma@esbt.vn>
2024-07-26 16:48:31 +0700

    26/7

---
+++ project_26/script.js
@@ -0,0 +1,24 @@
+const button = document.getElementById('button')
+const toasts = document.getElementById('.toasts')
+
+
+const messages = [
+    'message one',
+    'message two',
+    'message three',
+    'message four'
+]
+
+button.addEventListener('click', () => createNotification())
+
+function createNotification() {
+    const noti = document.createElement('div')
+    noti.classList.add('toast')
+
+    noti.innerText = getRamdomMessage()
+    console.log(noti)
+    toasts.appendChild(noti)
+}
+function getRamdomMessage(){
+    return messages[Math.floor(Math.random() * messages.length)]
+}
